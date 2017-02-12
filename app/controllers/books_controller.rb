2984b4f71bb79acf5e005c13a105d6702c02f92b
Dgibbins <nayr_ryan@hotmail.com>
2017-02-12 16:20:35 -0500

    Added Responses

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,59 @@
 class BooksController < ApplicationController
 
+  def index
+    @books = Book.all
 
+    respond_to do |format|
+      format.html
+      format.text
+      format.csv do
+        render plain: Book.generate_csv(@books)
+      end
+      format.json do
+        render json: @books
+      end
+    end
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to :index
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+
+    if @book.update_attributes(book_params)
+      redirect_to book_path(@book)
+    else
+      render :edit
+    end
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :author, :already_read)
+  end
 end
